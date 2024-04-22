e61b82f8d47f1a29d83177a743825ebf008df820
M-Bln <mlnd2@protonmail.com>
2024-04-22 16:06:51 +0200

    remove commented-out code

--- src/web3-auth.tsx
+++ src/web3-auth.tsx
@@ -19,24 +19,14 @@ export const Web3Auth : React.FC<Web3AuthProps> = ({provider, signer}) => {
 
     const signWeb3Auth = async () => {
         const address = await signer.getAddress();
-        console.log("address: ", address);
         const signature = await signer.signMessage(address);
-        console.log("signature: ", signature);
         setAuthHeader(`eth-${address}:${signature}`);
-        console.log("authHeader: ", `eth-${address}:${signature}`);
-        console.log("file: ", file);
-        //const newAuthHeader = Buffer.from(`eth-${address}:${signature}`).toString('base64');
-        console.log("eth: ", Buffer.from(`eth-${address}:${signature}`).toString('base64'));
-        console.log("     ", 'ZXRoLTB4ZjBhNWI1MzJmYzJhNWQ4ZTMyNGNjMmQ3YzYxZGJmZGMxMDBkMzkxZToweGIzMjBmNjRiZDM1NjExNzE0NmQzMGU2ZjkwZjNiYzMzNWIyYjU5N2JiNDMzODJiYWM0OWI3ZDgyMDk5ZGYxMTQwYjBmNzhiODY3NjM1M2QwMzU0MTE2NDdlYzM0OGI0MTIwMDM3ZTliMTQ2MTBkMGNjY2ZmMDkwMDUzOWRkMWVkMWM=')
-        console.log("sub: ", Buffer.from(`sub-${address}:${signature}`).toString('base64'));
-        console.log("     ", 'ZXRoLTB4ZjBhNWI1MzJmYzJhNWQ4ZTMyNGNjMmQ3YzYxZGJmZGMxMDBkMzkxZToweGIzMjBmNjRiZDM1NjExNzE0NmQzMGU2ZjkwZjNiYzMzNWIyYjU5N2JiNDMzODJiYWM0OWI3ZDgyMDk5ZGYxMTQwYjBmNzhiODY3NjM1M2QwMzU0MTE2NDdlYzM0OGI0MTIwMDM3ZTliMTQ2MTBkMGNjY2ZmMDkwMDUzOWRkMWVkMWM=')
-        console.log("sol: ", Buffer.from(`sol-${address}:${signature}`).toString('base64')); 
         // IPFS Web3 Authed Gateway address
         const ipfsGateway = 'https://crustipfs.xyz'
-        // const ipfsGateway = 'https://cors-anywhere.herokuapp.com/https://crustipfs.xyz'
+
 
         console.log("authHeader: ", Buffer.from(`eth-${address}:${signature}`).toString('base64'));
-        // 3. Create ipfs http client
+        // Create ipfs http client
         const ipfs = create({
             url: ipfsGateway + '/api/v0',
             headers: {
@@ -55,34 +45,6 @@ export const Web3Auth : React.FC<Web3AuthProps> = ({provider, signer}) => {
         } catch (error) {
             console.error(error);
         }
-
-        // const helia = await createHelia();
-        // const fs = unixfs(helia);
-        // const cid = await fs.addBytes(file)
-
-
-        // const pinServiceConfig = new Configuration({
-        //     endpointUrl: ipfsGateway, // the URI for your pinning provider, e.g. `http://localhost:3000`
-        //     accessToken: authHeader?authHeader:"" // the secret token/key given to you by your pinning provider
-        // });
-
-        // const remotePinningClient = new RemotePinningServiceClient(pinServiceConfig);
-        // const remotePinner = createRemotePinner(helia, remotePinningClient);
-
-        // //const { cid } = await ipfs.add(globSource('/home/mlnd42/Téléchargements/test_dir', 'test-file.txt'));
-
-        // if (cid) {
-        //     console.log('cid link: ', cid.link());
-        //     console.log('cid toString: ', cid.toString());
-        //     console.log('cid to V1: ', cid.toV1());
-        //     const addPinResult = await remotePinner.addPin({
-        //     cid,
-        //     name: 'helloWorld'
-        //     })
-        // } else {
-        //     throw new Error('IPFS add failed, please try again.');
-        // }
-
     }
     const handleFile = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
         const fileToUpload = e.target.files?.[0];
